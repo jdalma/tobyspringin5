684b7896a66416f722fa84b68d66017a9f7a1d12
정현준 <jeongdalma@gmail.com>
2022-09-28 23:25:09 +0900

    DB 정보 ID , PASSWORD 수정

--- src/main/java/springbook/chapter03/DaoFactory.java
+++ src/main/java/springbook/chapter03/DaoFactory.java
@@ -10,8 +10,8 @@ import javax.sql.DataSource;
 public class DaoFactory {
 
     private final String URL = "jdbc:mysql://localhost/springbook?autoReconnect=true&useSSL=false&serverTimezone=UTC";
-    private final String ID = "springbook";
-    private final String PASSWORD = "springbook!@";
+    private final String ID = "spring";
+    private final String PASSWORD = "book";
 
     @Bean
     public UserDao userDao(){
