8d156bd4d0c151ac91b564deec32fd2485dd6369
정현준 <jeongdalma@gmail.com>
2022-09-28 23:25:42 +0900

    3.3.1 add 전략 추가

--- src/main/java/springbook/chapter03/UserDao.java
+++ src/main/java/springbook/chapter03/UserDao.java
@@ -17,16 +17,8 @@ public class UserDao {
     }
 
     public void add(User user) throws SQLException{
-        Connection con = dataSource.getConnection();
-        PreparedStatement ps = con.prepareStatement("insert into users(id , name , password) values(? , ? , ?)");
-        ps.setString(1 , user.getId());
-        ps.setString(2 , user.getName());
-        ps.setString(3 , user.getPassword());
-
-        ps.executeUpdate();
-
-        ps.close();
-        con.close();
+        StatementStrategy st = new AddStatement(user);
+        jdbcContextWithStatementStrategy(st);
     }
 
     public User get(String id) throws SQLException {
