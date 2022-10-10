b48b5c797b6e58b091afeb0305c7ab4662b9b9cb
정현준 <jeongdalma@gmail.com>
2022-10-10 19:11:11 +0900

    5.4.1 JavaMail을 이용한 메일 발송 기능

--- src/main/java/springbook/chapter05/User.java
+++ src/main/java/springbook/chapter05/User.java
@@ -1,5 +1,12 @@
 package springbook.chapter05;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
 public class User {
     String id;
     String name;
@@ -37,71 +44,5 @@ public class User {
         else {
             this.level = nextLevel;
         }
-    }
-
-    @Override
-    public String toString() {
-        final StringBuilder sb = new StringBuilder("User{");
-        sb.append("id='").append(id).append('\'');
-        sb.append(", name='").append(name).append('\'');
-        sb.append(", password='").append(password).append('\'');
-        sb.append('}');
-        return sb.toString();
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public Level getLevel() {
-        return level;
-    }
-
-    public int getLogin() {
-        return login;
-    }
-
-    public int getRecommend() {
-        return recommend;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setLevel(Level level) {
-        this.level = level;
-    }
-
-    public void setLogin(int login) {
-        this.login = login;
-    }
-
-    public void setRecommend(int recommend) {
-        this.recommend = recommend;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 }
--- src/main/java/springbook/chapter05/UserLevelService.java
+++ src/main/java/springbook/chapter05/UserLevelService.java
@@ -1,5 +1,13 @@
 package springbook.chapter05;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
 import static springbook.chapter05.UserService.MIN_LOGIN_COUNT_FOR_SILVER;
 import static springbook.chapter05.UserService.MIN_RECOMMEND_FOR_GOLD;
 
@@ -28,5 +36,25 @@ public class UserLevelService implements UserLevelUpgradePolicy {
     public void upgradeLevel(User user) {
         user.upgradeLevel();
         userDao.update(user);
+        sendUpgradeEmail(user);
+    }
+
+    private void sendUpgradeEmail(User user) {
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "mail.ksug.org");
+        Session s = Session.getInstance(props, null);
+
+        MimeMessage message = new MimeMessage(s);
+        try {
+            message.setFrom(new InternetAddress("useradmin@ksug.org"));
+            message.addRecipient(Message.RecipientType.TO,
+                                new InternetAddress(user.getEmail()));
+            message.setSubject("Upgrade 안내");
+            message.setText(String.format("사용자님의 등급이 %s로 업그레이드 되었습니다.", user.getLevel().name()));
+
+            Transport.send(message);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
     }
 }
