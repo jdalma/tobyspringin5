88279bc7bf7322ce1c835e59fbd07b044a2550e8
정현준 <jeongdalma@gmail.com>
2022-10-15 19:06:17 +0900

    User필드 추가로 인해 테이블 수정

--- sql/users_create.sql
+++ sql/users_create.sql
@@ -1,5 +1,9 @@
 create table users (
 	id varchar(10) primary key,	
 	name varchar(20) not null,
-	password varchar(10) not null
+	password varchar(10) not null,
+	level int not null,
+	login int not null,
+	recommend int not null,
+	email varchar(50) not null
 )
