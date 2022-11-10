61a8b3a070fd899690fac4da5cc89ae7f7b6c910
정현준 <jeongdalma@gmail.com>
2022-11-10 12:38:16 +0900

    흩어진 설정 정보 AppContext 내부 스태틱 중첩 클래스로 이동

--- src/main/java/springbook/chapter07/AppContext.java
+++ src/main/java/springbook/chapter07/AppContext.java
@@ -5,31 +5,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.mail.MailSender;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
-import org.springframework.oxm.Unmarshaller;
-import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import springbook.chapter06.DummyMailSender;
-import springbook.chapter07.sqlService.EmbeddedDbSqlRegistry;
-import springbook.chapter07.sqlService.JaxbXmlSqlReader;
-import springbook.chapter07.sqlService.OxmSqlService;
-import springbook.chapter07.sqlService.SqlReader;
-import springbook.chapter07.sqlService.SqlRegistry;
 
 import javax.sql.DataSource;
 
 @Configuration
 @EnableTransactionManagement
 @ComponentScan(basePackages = "springbook.chapter07")
-@Import({SqlServiceContext.class, TestAppContext.class, ProductionAppContext.class})
+@Import(SqlServiceContext.class)
 public class AppContext {
 
     private final String URL = "jdbc:mysql://localhost/springbook?characterEncoding=UTF-8";
@@ -52,4 +42,37 @@ public class AppContext {
 
         return dataSource;
     }
+
+    @Configuration
+    @Profile("test")
+    public static class TestAppContext {
+
+        @Autowired
+        private UserDao userDao;
+
+        @Bean
+        public UserService userOnlyTestServiceImpl() {
+            TestUserService testService = new TestUserService();
+            testService.setUserDao(this.userDao);
+            testService.setMailSender(mailSenderImpl());
+            return testService;
+        }
+
+        @Bean
+        public MailSender mailSenderImpl() {
+            return new DummyMailSender();
+        }
+    }
+
+    @Configuration
+    @Profile("production")
+    public static class ProductionAppContext {
+        @Bean
+        public MailSender mailSenderImpl() {
+            JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+            mailSender.setHost("mail.test.com");
+            return mailSender;
+        }
+    }
+
 }
