f50dd3fc4367cf58a65382f5606c41c1ad8f7d90
정현준 <jeongdalma@gmail.com>
2022-11-10 13:49:51 +0900

    프로퍼티 정보 Value 어노테이션 적용

--- src/main/java/springbook/chapter07/AppContext.java
+++ src/main/java/springbook/chapter07/AppContext.java
@@ -1,13 +1,14 @@
 package springbook.chapter07;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.mail.MailSender;
@@ -23,11 +24,13 @@ import java.sql.Driver;
 @EnableTransactionManagement
 @ComponentScan(basePackages = "springbook.chapter07")
 @Import(SqlServiceContext.class)
-@PropertySource("database.properties")
+@PropertySource("/database.properties")
 public class AppContext {
 
-    @Autowired
-    Environment env;
+    @Value("${db.driverClass}") Class<? extends Driver> driverClass;
+    @Value("${db.url}") String url;
+    @Value("${db.username}") String username;
+    @Value("${db.password}") String password;
 
     @Bean
     public PlatformTransactionManager transactionManager() {
@@ -35,17 +38,17 @@ public class AppContext {
     }
 
     @Bean
+    public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+
+    @Bean
     public DataSource dataSource(){
         SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
-        try {
-            dataSource.setDriverClass((Class<? extends Driver>) Class.forName(env.getProperty("db.driverClass")));
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-
-        dataSource.setUrl(env.getProperty("db.url"));
-        dataSource.setUsername(env.getProperty("db.username"));
-        dataSource.setPassword(env.getProperty("db.password"));
+        dataSource.setDriverClass(this.driverClass);
+        dataSource.setUrl(this.url);
+        dataSource.setUsername(this.username);
+        dataSource.setPassword(this.password);
 
         return dataSource;
     }
